26c184e0d1b3f9eb3b3bf540a45b85c1970f823a
joaopauloramos <joaozimvip@gmail.com>
2017-05-24 01:17:13 -0300

    ADD Travis

--- .pypratico/bin/activate.fish
+++ .pypratico/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/jpramos/PycharmProjects/pypratico/.pypratico"
+set -gx VIRTUAL_ENV "/Users/jpramos/PycharmProjects/pypratico/.pypratico"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
