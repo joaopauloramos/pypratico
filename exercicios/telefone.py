6fdc48102156f55177a6cefb39209c6694b1781b
joaopauloramos <joaozimvip@gmail.com>
2017-06-04 17:18:46 -0300

    Lancamento de novos Testes

--- exercicios/telefone.py
+++ exercicios/telefone.py
@@ -1,3 +1,17 @@
+class RediscarExcecao(Exception):
+    pass
+
+
 class Telefone:
+    def __init__(self):
+        self.numero_anterior = None
+
     def ligar(self, numero):
+        self.numero_anterior = numero
         return f'ligar para {numero}'
+
+    def rediscar(self):
+        if self.numero_anterior == None:
+            raise RediscarExcecao
+
+        return self.ligar(self.numero_anterior)
--- tests/test_telefone.py
+++ tests/test_telefone.py
@@ -1,11 +1,31 @@
-from exercicios.telefone import Telefone
+import pytest
+from exercicios.telefone import Telefone, RediscarExcecao
 
 
 def test_telefone_init():
     assert Telefone() is not None
 
+NUMEROS_VALIDOS = ['8754432', 2345678, '2345678']
 
-def test_ligar():
+@pytest.mark.parametrize('numero', NUMEROS_VALIDOS)
+def test_ligar(numero):
     telefone = Telefone()
-    assert 'ligar para 2345678' == \
-           telefone.ligar(2345678)
+    assert 'ligar para ' + str(numero) == \
+           telefone.ligar(numero)
+
+@pytest.mark.parametrize('numero', NUMEROS_VALIDOS)
+def test_rediscar(numero):
+    telefone = Telefone()
+    telefone.ligar(numero)
+    assert 'ligar para ' + str(numero) \
+           == telefone.rediscar()
+
+def test_rediscar_excecao():
+    """Certifica que tentar rediscar antes de fazer uma ligação lança excecção"""
+    telefone = Telefone()
+    try:
+        telefone.rediscar()
+    except RediscarExcecao:
+        pass # Deu certo
+    else:
+        pytest.fail('Rediscar deveria ser lancado excecao')
